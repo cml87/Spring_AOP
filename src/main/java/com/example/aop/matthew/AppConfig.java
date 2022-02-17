3c7e5ce48f18bf7e9f9125c6b4f79754950ec14c
camilo.perez <camilo.perez@kaleyra.com>
2022-02-17 10:05:14 +0100

    add notes

---
+++ src/main/java/com/example/aop/matthew/AppConfig.java
@@ -0,0 +1,2 @@
+package com.example.aop.matthew;public class AppConfig {
+}
---
+++ src/main/java/com/example/aop/matthew/MyAspect.java
@@ -0,0 +1,31 @@
+package com.example.aop.matthew;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component // aspects need to be Spring managed beans as well!
+public class MyAspect {
+
+    // the class MyService will be looked for in this package
+    @Before("execution(void MyService.doSomething(..))")
+    public void intercept(){
+        System.out.println("Intercepted!");
+    }
+
+    // intercept all methods
+    @Before("execution(void MyService.*(..))")
+    public void intercept2(){
+        System.out.println("Intercepted all!");
+    }
+
+    @After("execution(void MyService.*(int))")
+    public void intercept3(JoinPoint joinPoint){
+        System.out.println("Intercepted!");
+    }
+
+}
+
